650c48c9a366789a410f31568115dc5888080e3f
Bob Gendler <boberito@mac.com>
2020-11-03 11:28:04 -0500

    fixed with arg parser

--- Sources/controlfreak_lookup/main.swift
+++ Sources/controlfreak_lookup/main.swift
@@ -9,21 +9,6 @@
 import Foundation
 
 
-let arguments = CommandLine.arguments
-var controlfreak = ControlLookup()
+Args.main()
 
-if arguments.count < 2 {
-    print("The following arguments are required: control")
-    exit(0)
-}
 
-if arguments[1] == "-h" {
-    print("Enter an 800-53r4 control to see if it's description and if it's on a baseline")
-    exit(0)
-}
-
-//if arguments[1] == "-r5" {
-    //
-//}
-
-controlfreak.lookup(control: arguments[1].uppercased())
