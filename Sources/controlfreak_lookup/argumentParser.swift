69a88b005b2c647d7b7fd769a3d4f50994a93bf6
Bob <boberito@mac.com>
2022-04-12 11:51:41 -0400

    updated argument parse. changed foundation networking. removed rev4

--- Sources/controlfreak_lookup/argumentParser.swift
+++ Sources/controlfreak_lookup/argumentParser.swift
@@ -12,16 +12,16 @@ enum Revision: String, ExpressibleByArgument {
 
 struct Args: ParsableCommand {
     
-    @Argument(help: ArgumentHelp("NIST 800-53 Revision, r4 or r5", valueName: "r4 or r5")) var rev: Revision
-    @Argument(help: "800-53 control") var control:String
+//    @Argument(help: ArgumentHelp("NIST 800-53 Revision, r4 or r5", valueName: "r4 or r5")) var rev: Revision
+    @Argument(help: "800-53r5 control") var control:String
     @Flag var showAll = false
     
     func run() throws {
-        switch rev {
-        case .r4:
-            controlfreak.lookup(control: control.uppercased(), showAll)
-        case .r5:
+//        switch rev {
+//        case .r4:
+//            controlfreak.lookup(control: control.uppercased(), showAll)
+//        case .r5:
             controlfreak.lookup5(control: control.uppercased(), showAll)
-        }
+//        }
     }
 }
